2affaa5ef127b915a66bf07f502e32d2c1e28039
Julius <Julius>
2020-05-11 20:19:27 +0300

    Recursion and Cycle For :) FACTORIAL

--- src/Factorial_and_Fibonacci.java
+++ src/Factorial_and_Fibonacci.java
@@ -3,9 +3,28 @@
  * @author Julius
  */
 public class Factorial_and_Fibonacci {
+    
+    // Factorial: 5! Recursion
+    
+    public static int factorial(int number) {
+        if(number == 0) {
+            return 1;
+        }
+        return number * factorial(number - 1);
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
+     public static void main(String[] args) {
+        System.out.println("Recursion: " + factorial(6));
+        
+    // and Cycle for
+        int num = 7;
+        int sum = 1;
+    for(int i=1; i<=num; i++) {
+        sum = sum * i;
+
+    }
+         System.out.println("Factorial of number " + num + " are " + sum + " ( Cycle FOR )");
+        
     }
     
 }
