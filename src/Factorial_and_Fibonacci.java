0522c0e74c303a161d237bdf63ecd4472d1d25d3
Julius <Julius>
2020-05-11 21:43:50 +0300

    Fibonacci | if over 200 method return

--- src/Factorial_and_Fibonacci.java
+++ src/Factorial_and_Fibonacci.java
@@ -4,17 +4,41 @@
  */
 public class Factorial_and_Fibonacci {
     
-    // Factorial: 5! Recursion
+    // Factorial: number! Recursion
     
-    public static int factorial(int number) {
+    public static int Factorial(int number) {
         if(number == 0) {
             return 1;
         }
-        return number * factorial(number - 1);
+        return number * Factorial(number - 1);
     }
+    
+    // Fibonacci
+    
+    public static int Fibonacci() {
+
+         int num1 = 1;
+         int num2 = 1;
+         int plus = 0;
+         System.out.println(1);
+
+        for (int i = 0; i <=i; ++i) {
+            if(plus > 200) {
+                return num1;
+            }
+            plus = num1 + num2;
+            num1 = num2;
+            num2 = plus;
+            System.out.println(num1);
+
+        }
+        return plus;
+    }    
+    
 
      public static void main(String[] args) {
-        System.out.println("Recursion: " + factorial(6));
+        System.out.println("Factorial Recursion: " + Factorial(6));
+        
         
     // and Cycle for
         int num = 7;
@@ -24,6 +48,11 @@ public class Factorial_and_Fibonacci {
 
     }
          System.out.println("Factorial of number " + num + " are " + sum + " ( Cycle FOR )");
+         
+         
+         System.out.println("Fibonacci: " + Fibonacci() + " next step over 200");
+         
+
         
     }
     
